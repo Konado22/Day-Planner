ad7bdf6e081b935fe0f7ab11a7a2627c663e8c87
Jesse Draper <jessedraper2@gmail.com>
2021-09-29 17:05:51 -0600

    test buttons and listeners and complete add events

--- script.js
+++ script.js
@@ -7,35 +7,53 @@ var evolve = setInterval( () =>{
      var alerts = prompt("Please enter meeting information");
      $('#item1').text(alerts)
    })
+   $('#btn2').on('click', function (){
+    var alerts = prompt("Please enter meeting information");
+    $('#item2').text(alerts)
+  })
+  $('#btn3').on('click', function (){
+    var alerts = prompt("Please enter meeting information");
+    $('#item3').text(alerts)
+  })
+  $('#btn4').on('click', function (){
+    var alerts = prompt("Please enter meeting information");
+    $('#item4').text(alerts)
+  })
+  $('#btn5').on('click', function (){
+    var alerts = prompt("Please enter meeting information");
+    $('#item5').text(alerts)
+  })
+  $('#btn6').on('click', function (){
+    var alerts = prompt("Please enter meeting information");
+    $('#item6').text(alerts)
+  })
+  $('#btn7').on('click', function (){
+    var alerts = prompt("Please enter meeting information");
+    $('#item7').text(alerts)
+  })
+  $('#btn8').on('click', function (){
+    var alerts = prompt("Please enter meeting information");
+    $('#item8').text(alerts)
+  })
+  $('#btn9').on('click', function (){
+    var alerts = prompt("Please enter meeting information");
+    $('#item9').text(alerts)
+  })
+  $('#btn10').on('click', function (){
+    var alerts = prompt("Please enter meeting information");
+    $('#item10').text(alerts)
+  })
+  $('#btn11').on('click', function (){
+    var alerts = prompt("Please enter meeting information");
+    $('#item11').text(alerts)
+  })
+  $('#btn12').on('click', function (){
+    var alerts = prompt("Please enter meeting information");
+    $('#item12').text(alerts)
+  })
+  $('#btn13').on('click', function (){
+    var alerts = prompt("Please enter meeting information");
+    $('#item13').text(alerts)
+  })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // btn1.onclick= function () {
-    //     $('#btn1').on( 'click', function(){
-    //       // window.prompt("Please enter meeting information");
-    //       var alerts = window.prompt("Please enter meeting information")
-    //       $('#item1').text(alerts)
-    //     })};
-      
-      // function myFunction() {
-      //   $('item1').html(clickEL)
-      // }
      
