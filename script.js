0520c2c015ea6005e8eba3397af27b314eee21c8
Jesse Draper <jessedraper2@gmail.com>
2021-09-29 16:55:27 -0600

    add event listeners and update hmtl further to test

--- script.js
+++ script.js
@@ -1,4 +1,41 @@
- var evolve = setInterval( () =>{
+var btn1 =$('#btn1')
+var evolve = setInterval( () =>{
     var now = moment().format('MMMM Do YYYY, h:mm:ss a')
     $('#currentDay').html("Today is : " + now)
     }, 1000);
+   $('#btn1').on('click', function (){
+     var alerts = prompt("Please enter meeting information");
+     $('#item1').text(alerts)
+   })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // btn1.onclick= function () {
+    //     $('#btn1').on( 'click', function(){
+    //       // window.prompt("Please enter meeting information");
+    //       var alerts = window.prompt("Please enter meeting information")
+    //       $('#item1').text(alerts)
+    //     })};
+      
+      // function myFunction() {
+      //   $('item1').html(clickEL)
+      // }
+     
